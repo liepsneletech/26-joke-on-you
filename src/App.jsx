20ab2c8896b03710a54b4de6df704eeb701fa60e
liepsnele <liepsnele.tech@gmail.com>
2022-12-11 15:01:23 +0200

    Implemented checking for errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,46 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.scss";
 import Card from "./Components/Card";
 
 function App() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
   const [cards, setCards] = useState([]);
 
-  useEffect(() => {
-    fetch("https://v2.jokeapi.dev/joke/Programming?amount=10")
-      .then((res) => res.json())
-      .then((data) => setCards(data.jokes));
-  }, []);
+  useEffect(
+    () => {
+      fetch("https://v2.jokeapi.dev/joke/Programming?amount=10")
+        .then((res) => res.json())
+        .then((data) => {
+          setIsLoaded(true);
+          setCards(data.jokes);
+        });
+    },
+    (error) => {
+      setIsLoaded(true);
+      setError(error);
+    },
+    []
+  );
 
-  return (
-    <div className="App">
-      <div className="container">
-        <h1 className="main-title">Jokes on you</h1>
-        <div className="cards-box">
-          {cards?.map((c, i) => {
-            return <Card key={i} {...c} />;
-          })}
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <div className="App">
+        <div className="container">
+          <h1 className="main-title">Jokes on you</h1>
+          <div className="cards-box">
+            {cards?.map((c, i) => {
+              return <Card key={i} {...c} />;
+            })}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ src/test.jsx
@@ -0,0 +1,33 @@
+useEffect(() => {
+  fetch("https://api.example.com/items")
+    .then((res) => res.json())
+    .then(
+      (result) => {
+        setIsLoaded(true);
+        setItems(result);
+      },
+      // Note: it's important to handle errors here
+      // instead of a catch() block so that we don't swallow
+      // exceptions from actual bugs in components.
+      (error) => {
+        setIsLoaded(true);
+        setError(error);
+      }
+    );
+}, []);
+
+if (error) {
+  return <div>Error: {error.message}</div>;
+} else if (!isLoaded) {
+  return <div>Loading...</div>;
+} else {
+  return (
+    <ul>
+      {items.map((item) => (
+        <li key={item.id}>
+          {item.name} {item.price}
+        </li>
+      ))}
+    </ul>
+  );
+}
